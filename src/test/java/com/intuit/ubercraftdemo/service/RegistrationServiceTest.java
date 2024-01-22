75ff36e72e71707e47a35ece2fa5dd95d594abd2
Atul Kumar <atulkumartata@gmail.com>
2024-01-22 12:16:10 +0530

    Removed outdated test class.

--- src/test/java/com/intuit/ubercraftdemo/service/RegistrationServiceTest.java
+++ src/test/java/com/intuit/ubercraftdemo/service/RegistrationServiceTest.java
@@ -1,7 +1,9 @@
 package com.intuit.ubercraftdemo.service;
 
+import com.google.gson.Gson;
 import com.intuit.ubercraftdemo.DriverMapper;
 import com.intuit.ubercraftdemo.endpoint.driver.DriverRegistrationDTO;
+import com.intuit.ubercraftdemo.exception.InvalidStepModificationException;
 import com.intuit.ubercraftdemo.exception.NoSuchRecordException;
 import com.intuit.ubercraftdemo.model.*;
 import com.intuit.ubercraftdemo.model.repository.*;
@@ -10,11 +12,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Collections;
-import java.util.Optional;
+import java.lang.reflect.Type;
+import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -46,6 +51,11 @@ public class RegistrationServiceTest {
 
     @Mock
     DriverOnboardingProcessRepository driverOnboardingProcessRepository;
+
+    @Mock
+    BudgetEditionS3Repository budgetEditionS3Repository;
+    @Mock
+    Gson gson;
 
     @Mock
     DriverMapper driverMapper;
@@ -98,4 +108,79 @@ public class RegistrationServiceTest {
         verify(operationMarketRepository, times(1)).findByCountryStateCity(any(), any(), any());
         verify(driverRepository, times(1)).save(any());
     }
+
+
+    @Test
+    public void testUploadDocumentsAndUpdateFileIdInOnboardingStepThrowsExceptionIfStepIsNotCurrent() {
+
+        Integer driverId = 1;
+        MockMultipartFile file = new MockMultipartFile("file", "filename.txt","text/plain", "test data".getBytes());
+        DriverOnboardingProcess dop = new DriverOnboardingProcess();
+        dop.setDriverId(driverId);
+        dop.setCurrentStepNumber(2);
+        DriverOnboardingStep dos = new DriverOnboardingStep();
+        dos.setDriverId(driverId);
+        dos.setStepNumber(1);
+        dos.setStepName("Not Document verification");
+        dos.setStatus(StepStatus.DriverActionNeeded);
+        List<DriverOnboardingStep> driverOnboardingStepList = new ArrayList<>();
+
+        when(driverOnboardingProcessRepository.findByDriverId(eq(driverId))).thenReturn(Optional.of(dop));
+        when(driverOnboardingStepRepository.findAllByDriverId(eq(driverId))).thenReturn(driverOnboardingStepList);
+
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> registrationService.uploadDocumentsAndUpdateFileIdInOnboardingStep(driverId, file));
+    }
+//    @Test
+//    public void testUploadDocumentsAndUpdateFileIdInOnboardingStepIsValid() throws Exception {
+//
+//        Integer driverId = 1;
+//        MockMultipartFile file = new MockMultipartFile("file", "filename.txt","text/plain", "test data".getBytes());
+//        DriverOnboardingProcess dop = new DriverOnboardingProcess();
+//        dop.setDriverId(driverId);
+//        dop.setCurrentStepNumber(1);
+//        DriverOnboardingStep dos = new DriverOnboardingStep();
+//        dos.setDriverId(driverId);
+//        dos.setStepNumber(1);
+//        dos.setAttachments("name");
+//        dos.setStepName("Document verification");
+//        dos.setStatus(StepStatus.DriverActionNeeded);
+//        BudgetEditionS3 s3FileRecord = new BudgetEditionS3();
+//        s3FileRecord.setId(1);
+//        s3FileRecord.setName("name");
+//        s3FileRecord.setOriginalFilename("filename");
+//        s3FileRecord.setFileContent(new byte[10]);
+//        Map<String, String> attachments = new HashMap<>();
+//
+//        when(driverOnboardingProcessRepository.findByDriverId(eq(driverId))).thenReturn(Optional.of(dop));
+//        when(driverOnboardingStepRepository.findAllByDriverId(eq(driverId))).thenReturn(Arrays.asList(dos));
+//        when(budgetEditionS3Repository.saveAll(any())).thenReturn(Arrays.asList(s3FileRecord));
+//        when(gson.fromJson(any(String.class), any(Type.class))).thenReturn(attachments);
+//
+//        Map<String, String> result = registrationService.uploadDocumentsAndUpdateFileIdInOnboardingStep(driverId, file);
+//        assertThat(result.values()).containsOnly(file.getOriginalFilename());
+//    }
+//
+
+
+//    @Test
+//    public void testUploadDocumentsAndUpdateFileIdInOnboardingStepThrowsExceptionIfStatusIsNotDriverActionNeeded() {
+//        Integer driverId = 1;
+//        MockMultipartFile file = new MockMultipartFile("file", "filename.txt","text/plain", "test data".getBytes());
+//        DriverOnboardingProcess dop = new DriverOnboardingProcess();
+//        dop.setDriverId(driverId);
+//        dop.setCurrentStepNumber(2);
+//        DriverOnboardingStep dos = new DriverOnboardingStep();
+//        dos.setDriverId(driverId);
+//        dos.setStepNumber(1);
+//        dos.setStepName("Document verification");
+//        dos.setStatus(StepStatus.WaitingForAuditorAssignment);
+//        List<DriverOnboardingStep> driverOnboardingStepList = new ArrayList<>();
+//
+//        when(driverOnboardingProcessRepository.findByDriverId(eq(driverId))).thenReturn(Optional.of(dop));
+//        when(driverOnboardingStepRepository.findAllByDriverId(eq(driverId))).thenReturn(driverOnboardingStepList);
+//
+//        assertThatExceptionOfType(InvalidStepModificationException.class)
+//                .isThrownBy(() -> registrationService.uploadDocumentsAndUpdateFileIdInOnboardingStep(driverId, file));
+//    }
 }
